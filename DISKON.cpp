9eb61b880606e9f1d33944163ee9d7ebacd51dcf
reynara_putra_ramadhan <reynihbangg@gmail.com>
2025-11-20 11:40:42 +0100

    Add files via upload

---
+++ DISKON.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double harga, diskon, jumlahdiskon, hargasetelahdiskon;
+
+	cout << "=MASUKAN=" << endl;
+	cout << "harga barang(rp)=";
+	cin >> harga;
+
+	cout << "diskon barang (%)=";
+	cin >> diskon;
+
+	jumlahdiskon = harga * (diskon / 100);
+	hargasetelahdiskon = harga * (diskon / 100);
+
+	cout << "\nhasil:" << endl;
+	cout << "Jumlah Diskon        =RP" << jumlahdiskon << endl;
+	cout << "Harga setelah diskon   =RP" << hargasetelahdiskon << endl;
+
+	return 0;
+
+
+
+
+
+}
